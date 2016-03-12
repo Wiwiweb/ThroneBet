b220ea4f8bd0fa0119ee1e2b68b6f50e09a4dfd1
Wiwiweb <wiwiweb2@gmail.com>
2016-03-11 21:05:22 -0800

    Actually works now

--- server.js
+++ server.js
@@ -6,44 +6,45 @@ var request = require('request');
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-var previousLastHit = 0;
+var previousHealth = 0;
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
 
 
-http.listen(server_port, server_ip_address, function() {
+http.listen(server_port, server_ip_address, function () {
     console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
 
 setInterval(getThroneData, 1000);
 
 function getThroneData() {
-    var url = 'https://tb-api.xyz/stream/get?s=76561197980980302&key=CGKNPQTWY';
-    request.get(url, function(error, response, body) {
+    request.get(wiwi, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var info = JSON.parse(body);
             if (info['current'] != null) {
-                var currentLastHit = info['current']['lastHit'];
-                if (currentLastHit == previousLastHit) {
-                    if (info['current']['health'] == 0) {
-                        console.info("dead: " + currentLastHit);
-                        previousLastHit = 0;
-                        io.emit('dead', currentLastHit);
-                    } else {
-                        console.info("hurt: " + currentLastHit);
-                        previousLastHit = currentLastHit;
-                        io.emit('hurt', currentLastHit)
-                    }
+                var currentLastHit = info['current']['lasthit'];
+                console.info("currentLastHit: " + currentLastHit);
+                if (info['current']['health'] < previousHealth) {
+                    console.info("hurt: " + currentLastHit);
+                    io.emit('hurt', currentLastHit)
                 }
+                previousHealth = info['current']['health']
+            } else if (info['previous'] != null
+                && info['previous']['health'] == 0
+                && info['previous']['health'] < previousHealth) {
+                previousHealth = 0;
+                var previousLastHit = info['previous']['lasthit'];
+                console.info("dead: " + previousLastHit);
+                io.emit('dead', previousLastHit)
             }
         }
     });
