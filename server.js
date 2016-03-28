926b9c369cba6c32dce1f5b265c96bbaf4371202
Wiwiweb <wiwiweb2@gmail.com>
2016-03-28 12:18:55 -0700

    Channel creation and joining Huge commit!

--- server.js
+++ server.js
@@ -10,52 +10,99 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var previousHealth = 0;
 
 var userList = {}; // Dictionary of user id -> channel the user is in
-var channelList = {}; // Dictionary of channels -> list of users in channel
+var channelList = {}; // Dictionary of channel name -> channel object (contains key, list of users)
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
 });
 
-app.get('/client.js', function (req, res) {
-    res.sendFile(__dirname + '/client.js');
+app.get('/channel/[a-z0-9]+', function(req, res) {
+    res.sendFile(__dirname + '/client/channel.html');
 });
 
-io.on('connection', function (socket) {
+app.get('/index.js', function(req, res) {
+    res.sendFile(__dirname + '/client/index.js');
+});
+
+app.get('/channel/channel.js', function(req, res) {
+    res.sendFile(__dirname + '/client/channel.js');
+});
+
+app.get('/main.css', function(req, res) {
+    res.sendFile(__dirname + '/client/main.css');
+});
+
+io.on('connection', function(socket) {
     console.log("User " + socket.id + " connected");
-    socket.on('join channel', function (channel) {
-        addUserToChannel(socket, channel);
-        socket.on('disconnect', function () {
-            disconnectUser(socket.id)
-        });
+    socket.on('create channel', function(channel, key) {
+        createChannel(socket, channel, key);
+    });
+    socket.on('check channel valid', function(channel) {
+        if (channelList[channel]) {
+            io.to(socket.id).emit('channel valid', channel);
+        } else {
+            io.to(socket.id).emit('error', "Channel does not exist!");
+        }
+    });
+    socket.on('join channel', function(channel) {
+        if (channelList[channel]) {
+            addUserToChannel(socket, channel);
+        } else {
+            io.to(socket.id).emit('error', "Channel does not exist!");
+        }
     });
 });
+
+function createChannel(socket, channel, key) {
+    getThroneData(channel, key, function(error, data) {
+        if (error) {
+            console.log("Channel " + channel + " could not be created, " + error);
+            if (error.message == 403) {
+                io.to(socket.id).emit('error', "Wrong channel and/or key!");
+            } else {
+                io.to(socket.id).emit('error', "Unknown error!");
+            }
+            return;
+        }
+        console.log("Creating channel " + channel);
+        channelList[channel] = {'key': key, 'users': []};
+        io.to(socket.id).emit('channel valid', channel);
+    });
+}
 
 function addUserToChannel(socket, channel) {
     var user = socket.id;
     console.log("User " + user + " joined channel " + channel);
     userList[user] = channel;
     if (!channelList[channel]) {
-        console.log('Creating channel ' + channel);
-        channelList[channel] = [];
-        socket.join(channel)
+        console.error("Channel doesn't exist! (this shouldn't happen)");
     }
-    channelList[channel].push(user)
+    socket.join(channel);
+    channelList[channel]['users'].push(user);
+    socket.on('disconnect', function() {
+        disconnectUser(socket.id)
+    });
+    io.to(socket.id).emit('connected');
 }
 
 function disconnectUser(user) {
     console.log("User " + user + " disconnected");
     var channel = userList[user];
     delete userList[user];
-    channelList[channel].splice(channelList[channel].indexOf(user), 1); // Remove user from list
+    if (!channelList[channel]) {
+        console.error("Channel doesn't exist! (this shouldn't happen)");
+    }
+    // Remove user from list
+    channelList[channel]['users'].splice(channelList[channel]['users'].indexOf(user), 1);
 
-    if (channelList[channel].length == 0) {
+    if (channelList[channel]['users'].length == 0) {
         console.log("Channel " + channel + " is now empty, removing");
         delete channelList[channel];
     }
 }
 
 
-http.listen(server_port, server_ip_address, function () {
+http.listen(server_port, server_ip_address, function() {
     console.log("Listening on http://" + server_ip_address + ":" + server_port)
 });
 
@@ -64,33 +111,45 @@ setInterval(mainLoop, 1000);
 function mainLoop() {
     for (var channel in channelList) {
         if (channelList.hasOwnProperty(channel)) { // Necessary to avoid looping over prototype properties
-            getThroneData(channel)
+            var data = getThroneData(channel, channelList[channel]['key'], function(error, data) {
+                if (error) {
+                    console.error("Error fetching Throne data! code: " + error);
+                    return;
+                }
+                sendEventNotifications(data);
+            });
         }
     }
 }
 
-function getThroneData(channel) {
+function getThroneData(channel, key, callback) {
     console.log("Checking data for channel " + channel);
-    var url = 'https://tb-api.xyz/stream/get?' + channel;
-    request.get(url, function (error, response, body) {
+    var url = 'https://tb-api.xyz/stream/get?s=' + channel + '&key=' + key;
+    request.get(url, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body);
-            if (info['current'] != null) {
-                var currentLastHit = info['current']['lasthit'];
-                console.info("currentLastHit: " + currentLastHit);
-                if (info['current']['health'] < previousHealth) {
-                    console.info("hurt: " + enemy[currentLastHit]);
-                    io.to(channel).emit('hurt', enemy[currentLastHit])
-                }
-                previousHealth = info['current']['health']
-            } else if (info['previous'] != null
-                && info['previous']['health'] == 0
-                && info['previous']['health'] < previousHealth) {
-                previousHealth = 0;
-                var previousLastHit = info['previous']['lasthit'];
-                console.info("dead: " + enemy[previousLastHit]);
-                io.to(channel).emit('dead', enemy[previousLastHit])
-            }
+            callback(null, JSON.parse(body));
+        } else {
+            console.log("Didn't work: " + response.statusCode);
+            callback(new Error(response.statusCode));
         }
     });
+}
+
+function sendEventNotifications(data) {
+    if (data['current'] != null) {
+        var currentLastHit = data['current']['lasthit'];
+        console.info("currentLastHit: " + currentLastHit);
+        if (data['current']['health'] < previousHealth) {
+            console.info("hurt: " + enemy[currentLastHit]);
+            io.to(channel).emit('hurt', enemy[currentLastHit])
+        }
+        previousHealth = data['current']['health']
+    } else if (data['previous'] != null
+        && data['previous']['health'] == 0
+        && data['previous']['health'] < previousHealth) {
+        previousHealth = 0;
+        var previousLastHit = data['previous']['lasthit'];
+        console.info("dead: " + enemy[previousLastHit]);
+        io.to(channel).emit('dead', enemy[previousLastHit])
+    }
 }
