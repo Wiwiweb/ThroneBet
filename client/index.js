19c1cb2d714f51e0c6d7ab2fbaaedd8db8b217d3
Wiwiweb <wiwiweb2@gmail.com>
2016-03-30 15:54:27 -0700

    Add missing index.js No idea what I did...

--- client/index.js
+++ client/index.js
@@ -1,3 +1,25 @@
-/**
- * Created by William on 26/3/2016.
- */
+var socket = io();
+
+$(document).ready(function() {
+
+    $('#createChannelForm').submit(function(event) {
+        console.debug('Create channel');
+        socket.emit('create channel', $('#createChannelInput').val(), $('#createKeyInput').val());
+        return false;
+    });
+
+    $('#joinChannelForm').submit(function(event) {
+        console.debug('Join channel');
+        socket.emit('check channel valid', $('#joinChannelInput').val());
+        return false;
+    });
+
+    socket.on('error', function(msg) {
+        alert(msg);
+    });
+
+    socket.on('channel valid', function(channel) {
+        window.location.href = '/channel/' + channel;
+    });
+
+});
