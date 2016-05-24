2f0aec324a14a546cb1dc11489a09028e6c19f38
Wiwiweb <wiwiweb2@gmail.com>
2016-05-24 14:24:29 -0700

    Add skeleton for session management

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('request');
+var session = require('express-session');
 var enemy = require('./enemyData');
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.use(session({
+    secret: 'foo'
+}));
+
 
 var previousHealth = 0;
 
