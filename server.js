7c425ea739818f3166e2d29341f5aaf4f1d08e6c
Wiwiweb <wiwiweb2@gmail.com>
2016-05-25 10:38:50 -0700

    Add missing session config

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.use(session({
-    secret: config['session_secret']
+    secret: config['session_secret'],
+    resave: false,
+    saveUninitialized: true
 }));
 
 
