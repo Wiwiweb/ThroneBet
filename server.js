faab176b2f644c2f8ac96e21688a830d65d996ac
Wiwiweb <wiwiweb2@gmail.com>
2016-06-16 23:39:19 -0700

    Reset database in debug mode

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ if (process.argv[2] == 'debug') {
             return dateFormat(new Date());
         }, 'colorize': true
     });
+    db("TRUNCATE sessions");
+    db("TRUNCATE users");
 }
 
 var serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
